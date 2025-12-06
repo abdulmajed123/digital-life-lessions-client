c1069ee0d05fe992dcc00518edb41fb6bf5affd5
abdulmajed123 <abdulmajed5618@gmail.com>
2025-12-06 08:33:55 +0600

    added navbar and firebase authentication

---
+++ src/Routes/Router.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { createBrowserRouter } from "react-router";
+import RootLayout from "../Layout/RootLayout";
+import Home from "../Pages/Home/Home";
+import Login from "../Pages/Login/Login";
+import Register from "../Pages/Register/Register";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout></RootLayout>,
+    children: [
+      {
+        index: true,
+        element: <Home></Home>,
+      },
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
+      {
+        path: "/register",
+        element: <Register></Register>,
+      },
+    ],
+  },
+]);
+
+export default router;
